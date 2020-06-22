7d198d0b0dce3e632734520ebe99f89aaa0fb8e7
tundera <stackshuffle@gmail.com>
2020-06-21 23:25:48 -0600

    added nitro

---
+++ apps/nitro/next.config.js
@@ -0,0 +1,22 @@
+// next.config.js
+const withPlugins = require('next-compose-plugins');
+const offline = require('next-offline');
+const withPreconstruct = require('@preconstruct/next');
+
+const nextConfig = {
+  target: process.env.NODE_ENV !== 'production' ? 'server' : 'serverless',
+  dontAutoRegisterSw: true,
+  generateSw: false,
+  devSwSrc: './static/sw.js',
+  workboxOpts: {
+    swSrc: './static/sw.js',
+    swDest: './static/service-worker.js',
+  },
+  // Exposes Server ENV Vars To Client Using Webpack
+  env: {
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  },
+};
+
+module.exports = withPreconstruct(withPlugins([[offline]], nextConfig));
